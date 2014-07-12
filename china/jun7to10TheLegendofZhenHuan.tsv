195de7868cbbc78cc8b80d82e0f1ab08f05b0c53
Akhil Kohli <akohli@tdv-grapx-akhil.local.tribunemedia.com>
2014-07-13 01:18:40 +0530

    commiting

---
+++ china/jun7to10TheLegendofZhenHuan.tsv
@@ -0,0 +1,4 @@
+date	totalTweet	positive	negative	neutral	time
+20140607	61	28	3	30	0
+20140608	159	36	121	2	0
+20140609	368	106	29	233	0
--- newtiwtterui/newui.js
+++ newtiwtterui/newui.js
@@ -23,6 +23,20 @@ function changeFunction()
      if($("#socialType :selected").text()=="TWITTER")
         {
                     $('#twitterButton').show();
+                     if($("#ddlViewBy :selected").text()=="TheCondorHeroes" || $("#ddlViewBy :selected").text()=="BuBuJingQing"
+                     ||$("#ddlViewBy :selected").text()=="LeJunKai"||$("#ddlViewBy :selected").text()=="TheLegendofZhenHuan")
+                        {
+                $('#chinad').show();
+                $('#usd').hide();
+
+
+                        }
+                        else
+                        {
+                $('#usd').show();
+                $('#chinad').hide();
+
+                        }
         }
     if($("#socialType :selected").text()=="TMS")
             {
@@ -103,7 +117,7 @@ d3.select("svg")
   $.get(file, function(data) {
       var lines = data.split("\n");
                 $.each(lines, function(n, item) {
-                  $('#multimedia').append('<div><p><img src="'+item+'"</p></div>');
+                  $('#multimedia').append('<div><p><img   src="'+item+'"</p></div>');
           });
       }, "text")
 
@@ -161,10 +175,13 @@ d3.select("svg")
   $.get(file, function(data) {
       var lines = data.split("\n");
                 $.each(lines, function(n, item) {
-                  $('#multimedia').append('<div><p><img src="'+item+'"</p></div>');
+                 $('#multimedia').append('<div><div class="image"><a href="#"><p><img    src="'+item+'"</p></a></div></div>');
+              //   $('#slideshow_1').append("<div class=\"slideshow_item\"><div class=\"image\"><a href=\"#\"><img  alt=\"photo 1\" width=\"900\" height=\"400\" src=\""+item+"\""
+               //   +"/></a></div></div>");
+
           });
       }, "text")
-
+        $('#multimedia').append("</div>");
 });
 
 $('#graph').click(function(){
@@ -345,6 +362,33 @@ d3.select("svg")
    $('#positivetweet').text('');
           $('#negativeTweet').text('');
 
+          if($("#ddlViewBy :selected").text()=="TheCondorHeroes" || $("#ddlViewBy :selected").text()=="BuBuJingQing"
+                               ||$("#ddlViewBy :selected").text()=="LeJunKai"||$("#ddlViewBy :selected").text()=="TheLegendofZhenHuan")
+                                  {
+                        var file="jun7to10"+$("#ddlViewBy :selected").text()+"alltext.tsv";
+                                            $('#tweet').append("<p class=\"text-center\"><h3> <p class=\"text-primary\">All Tweets</p></h3></p><br/>")
+                          $.get(file, function(data) {
+                              var lines = data.split("\n");
+                                        $.each(lines, function(n, item) {
+                                                $('#tweet').append('<div><p>' + item.linkify() + '</p></div>'+ '<div id="web_intent">' + '<span class="time">'  + '</span>' + '<img src="index.jpg" width="16" height="16" alt="Retweet">' + '<a href="http://twitter.com/intent/retweet?tweet_id=' + 1+ '">' + 'Retweet</a>' + '<img src="index.jpg" width="16" height="16" alt="Reply">' + '<a href="http://twitter.com/intent/tweet?in_reply_to=' + 1+ '">' + 'Reply</a>' + '<img src="index.jpg" width="16" height="16" alt="Favorite">' + '<a href="http://twitter.com/intent/favorite?tweet_id=' + 1 + '">' + 'Favorite</a>' + '</div>' + '<hr />');
+
+                                  });
+                              }, "text")
+
+                                                  $('#positivetweet').append("<p class=\"text-center\"><h3> <p class=\"text-primary\">Positive Tweets</p></h3></p><br/>")
+
+                               var file="jun7to10"+$("#ddlViewBy :selected").text()+"positivetext.tsv";
+                                $.get(file, function(data) {
+                                    var lines = data.split("\n");
+                                              $.each(lines, function(n, item) {
+                                                $('#positivetweet').append('<div><p>' + item.linkify() + '</p></div>'+ '<div id="web_intent">' + '<span class="time">'  + '</span>' + '<img src="index.jpg" width="16" height="16" alt="Retweet">' + '<a href="http://twitter.com/intent/retweet?tweet_id=' + 1+ '">' + 'Retweet</a>' + '<img src="index.jpg" width="16" height="16" alt="Reply">' + '<a href="http://twitter.com/intent/tweet?in_reply_to=' + 1+ '">' + 'Reply</a>' + '<img src="index.jpg" width="16" height="16" alt="Favorite">' + '<a href="http://twitter.com/intent/favorite?tweet_id=' + 1 + '">' + 'Favorite</a>' + '</div>' + '<hr />');
+
+                                        });
+                                    }, "text")
+                                  }
+                                  else
+                                  {
+
   var file="jun7to10"+$("#ddlViewBy :selected").text()+"alltext.tsv";
                     $('#tweet').append("<p class=\"text-center\"><h3> <p class=\"text-primary\">All Tweets</p></h3></p><br/>")
   $.get(file, function(data) {
@@ -367,11 +411,27 @@ d3.select("svg")
 
                 });
             }, "text")
+            }
 });
 
 
-$('#geo').click(function(){
+$('#usbutton').click(function(){
+d3.select("svg")
+       .remove();
+$('#maindiv').hide();
+      $('#newdiv').hide();
+         $('#onemorediv').show();
 
+   $('#tweet').text('');
+       $('#positivetweet').text('');
+       $('#negativeTweet').text('');
+         $('#chart_div').text('');
+              $('#chart_div').hide();
+    countryMapUS();});
+
+$('#geo').click(function(){
+d3.select("svg")
+       .remove();
 $('#maindiv').hide();
       $('#newdiv').hide();
                   $('#onemorediv').show();
@@ -384,6 +444,162 @@ $('#maindiv').hide();
               drawRegionsMap();
 
 });
+
+
+$('#chinabutton').click(function(){
+d3.select("svg")
+       .remove();
+$('#maindiv').hide();
+      $('#newdiv').hide();
+                  $('#onemorediv').show();
+
+   $('#tweet').text('');
+       $('#positivetweet').text('');
+       $('#negativeTweet').text('');
+         $('#chart_div').text('');
+              $('#chart_div').hide();
+                            countryMapIND();});
+
+function countryMapIND() {
+      var data = new google.visualization.DataTable();
+           data.addColumn('string', 'State');
+           data.addColumn('number', 'POPULARITY');
+if( $("#ddlViewBy :selected").text()=="BuBuJingQing")
+{
+data.addRow(['CN-31',19000]);data.addRow(['CN-12',6000]);
+data.addRow(['CN-34',7000]);data.addRow(['CN-35',4000]);
+data.addRow(['CN-62',1000]);data.addRow(['CN-44',3000]);
+data.addRow(['CN-52',1000]);data.addRow(['CN-46',2000]);
+data.addRow(['CN-13',3000]);data.addRow(['CN-23',1000]);
+data.addRow(['CN-41',2000]);data.addRow(['CN-42',1000]);
+data.addRow(['CN-43',1000]);data.addRow(['CN-32',200]);
+data.addRow(['CN-36',1000]);data.addRow(['CN-22',1000]);
+data.addRow(['CN-21',5000]);data.addRow(['CN-63',7000]);
+data.addRow(['CN-61',1000]);
+data.addRow(['CN-37',1000]);data.addRow(['CN-14',1000]);data.addRow(['CN-51',1000]);data.addRow(['CN-71',1000]);
+data.addRow(['CN-53',8000]);data.addRow(['CN-33',1000]);
+data.addRow(['CN-45',1000]);data.addRow(['CN-15',2000]);data.addRow(['CN-64',1000]);data.addRow(['CN-65',1000]);
+data.addRow(['CN-54',1000]);data.addRow(['CN-91',1000]);data.addRow(['CN-92',1000]);
+ data.addRow(['CN-71',1000]);data.addRow(['CN-91',1000]);
+data.addRow(['CN-92',1000]);
+}
+else if ($("#ddlViewBy :selected").text()=="TheLegendofZhenHuan")
+{
+data.addRow(['CN-31',10000]);data.addRow(['CN-12',6000]);
+data.addRow(['CN-34',3000]);data.addRow(['CN-35',4000]);
+data.addRow(['CN-62',1000]);data.addRow(['CN-44',3000]);
+data.addRow(['CN-52',5000]);data.addRow(['CN-46',2000]);
+data.addRow(['CN-13',3000]);data.addRow(['CN-23',1000]);
+data.addRow(['CN-41',2000]);data.addRow(['CN-42',1000]);
+data.addRow(['CN-43',100]);data.addRow(['CN-32',200]);
+data.addRow(['CN-36',1000]);data.addRow(['CN-22',1000]);
+data.addRow(['CN-21',600]);data.addRow(['CN-63',7000]);
+data.addRow(['CN-61',1000]);
+data.addRow(['CN-37',1000]);data.addRow(['CN-14',1000]);data.addRow(['CN-51',1000]);data.addRow(['CN-71',1000]);
+data.addRow(['CN-53',700]);data.addRow(['CN-33',1000]);
+data.addRow(['CN-45',1000]);data.addRow(['CN-15',800]);data.addRow(['CN-64',1000]);data.addRow(['CN-65',1000]);
+data.addRow(['CN-54',1000]);data.addRow(['CN-91',1000]);data.addRow(['CN-92',1000]);
+ data.addRow(['CN-71',1000]);data.addRow(['CN-91',1000]);
+data.addRow(['CN-92',1000]);
+
+}
+else if ($("#ddlViewBy :selected").text()=="LeJunKai")
+{
+data.addRow(['CN-31',7000]);data.addRow(['CN-12',6000]);
+data.addRow(['CN-34',7000]);data.addRow(['CN-35',4000]);
+data.addRow(['CN-62',1000]);data.addRow(['CN-44',400]);
+data.addRow(['CN-52',1000]);data.addRow(['CN-46',2000]);
+data.addRow(['CN-13',3000]);data.addRow(['CN-23',1000]);
+data.addRow(['CN-41',2000]);data.addRow(['CN-42',1000]);
+data.addRow(['CN-43',1000]);data.addRow(['CN-32',200]);
+data.addRow(['CN-36',1000]);data.addRow(['CN-22',1000]);
+data.addRow(['CN-21',5000]);data.addRow(['CN-63',7000]);
+data.addRow(['CN-61',1000]);
+data.addRow(['CN-37',1000]);data.addRow(['CN-14',1000]);data.addRow(['CN-51',1000]);data.addRow(['CN-71',1000]);
+data.addRow(['CN-53',8000]);data.addRow(['CN-33',1000]);
+data.addRow(['CN-45',1000]);data.addRow(['CN-15',600]);data.addRow(['CN-64',1000]);data.addRow(['CN-65',1000]);
+data.addRow(['CN-54',1000]);data.addRow(['CN-91',1000]);data.addRow(['CN-92',1000]);
+ data.addRow(['CN-71',1000]);data.addRow(['CN-91',1000]);
+data.addRow(['CN-92',1000]);
+}
+      var formatter = new google.visualization.NumberFormat({prefix: 'Popularity', fractionDigits: 0});
+            formatter.format(data, 1); // Apply formatter to second column
+            var stateHeatMap = new google.visualization.GeoChart(document.getElementById('visualization'));
+            stateHeatMap.draw(data, {width: 960, height: 480, region: 'CN', resolution: 'provinces', legend: {numberFormat: '#,###', textStyle: {color: 'blue', fontSize: 16}} });
+         }
+
+
+
+function countryMapUS() {
+      var data = new google.visualization.DataTable();
+           data.addColumn('string', 'State');
+           data.addColumn('number', 'Popularity');
+            data.addRow(['US-AL',Math.floor(Math.random() * 100)]);
+           data.addRow(['US-AK',Math.floor(Math.random() * 100)]);
+           data.addRow(['US-AZ',Math.floor(Math.random() * 100)]);
+           data.addRow(['US-AR',Math.floor(Math.random() * 100)]);
+           data.addRow(['US-CA',Math.floor(Math.random() * 100)]);
+           data.addRow(['US-CO',Math.floor(Math.random() * 100)]);
+           data.addRow(['US-CT',Math.floor(Math.random() * 100)]);
+           data.addRow(['US-DE',Math.floor(Math.random() * 100)]);
+           data.addRow(['US-DC',Math.floor(Math.random() * 100)]);
+           data.addRow(['US-FL',Math.floor(Math.random() * 100)]);
+           data.addRow(['US-GA',Math.floor(Math.random() * 100)]);
+           data.addRow(['US-HI',Math.floor(Math.random() * 100)]);
+           data.addRow(['US-ID',Math.floor(Math.random() * 100)]);
+           data.addRow(['US-IL',Math.floor(Math.random() * 100)]);
+           data.addRow(['US-IN',Math.floor(Math.random() * 100)]);
+           data.addRow(['US-IA',Math.floor(Math.random() * 100)]);
+           data.addRow(['US-KS',Math.floor(Math.random() * 100)]);
+           data.addRow(['US-KY',Math.floor(Math.random() * 100)]);
+           data.addRow(['US-LA',Math.floor(Math.random() * 100)]);
+           data.addRow(['US-ME',Math.floor(Math.random() * 100)]);
+           data.addRow(['US-MD',Math.floor(Math.random() * 100)]);
+           data.addRow(['US-MA',Math.floor(Math.random() * 100)]);
+           data.addRow(['US-MI',Math.floor(Math.random() * 100)]);
+           data.addRow(['US-MN',Math.floor(Math.random() * 100)]);
+           data.addRow(['US-MS',Math.floor(Math.random() * 100)]);
+           data.addRow(['US-MO',Math.floor(Math.random() * 100)]);
+           data.addRow(['US-MT',Math.floor(Math.random() * 100)]);
+           data.addRow(['US-NE',Math.floor(Math.random() * 100)]);
+           data.addRow(['US-NV',Math.floor(Math.random() * 100)]);
+           data.addRow(['US-NH',Math.floor(Math.random() * 100)]);
+           data.addRow(['US-NJ',Math.floor(Math.random() * 100)]);
+           data.addRow(['US-NM',Math.floor(Math.random() * 100)]);
+           data.addRow(['US-NY',Math.floor(Math.random() * 100)]);
+           data.addRow(['US-NC',Math.floor(Math.random() * 100)]);
+           data.addRow(['US-ND',Math.floor(Math.random() * 100)]);
+           data.addRow(['US-OH',Math.floor(Math.random() * 100)]);
+           data.addRow(['US-OK',Math.floor(Math.random() * 100)]);
+           data.addRow(['US-OR',Math.floor(Math.random() * 100)]);
+           data.addRow(['US-PA',Math.floor(Math.random() * 100)]);
+           data.addRow(['US-RI',Math.floor(Math.random() * 100)]);
+           data.addRow(['US-SC',Math.floor(Math.random() * 100)]);
+           data.addRow(['US-SD',Math.floor(Math.random() * 100)]);
+           data.addRow(['US-TN',Math.floor(Math.random() * 100)]);
+           data.addRow(['US-TX',Math.floor(Math.random() * 100)]);
+           data.addRow(['US-UT',Math.floor(Math.random() * 100)]);
+           data.addRow(['US-VT',Math.floor(Math.random() * 100)]);
+           data.addRow(['US-VA',Math.floor(Math.random() * 100)]);
+           data.addRow(['US-WA',Math.floor(Math.random() * 100)]);
+           data.addRow(['US-WV',Math.floor(Math.random() * 100)]);
+           data.addRow(['US-WI',Math.floor(Math.random() * 100)]);
+           data.addRow(['US-WY',200]);
+           data.addRow(['US-AS',440]);
+           data.addRow(['US-GU',330]);
+           data.addRow(['US-MP',880]);
+           data.addRow(['US-PR',900]);
+           data.addRow(['US-VI',100]);
+
+            var formatter = new google.visualization.NumberFormat({prefix: 'Popularity', fractionDigits: 0});
+            formatter.format(data, 1); // Apply formatter to second column
+            var stateHeatMap = new google.visualization.GeoChart(document.getElementById('visualization'));
+            stateHeatMap.draw(data, {width: 960, height: 500, region: 'US', resolution: 'provinces', legend: {numberFormat: '#,###', textStyle: {color: 'blue', fontSize: 16}} });
+         }
+
+
+
+
 
 function drawRegionsMap() {
 
@@ -399,7 +615,53 @@ function drawRegionsMap() {
                         ['RU', Math.floor(Math.random() * 600)],
                         ['INDIA', Math.floor(Math.random() * 400)]
 
+
+
                       ]);
+
+ if($("#ddlViewBy :selected").text()=="TheCondorHeroes")
+                                                      {
+                                                            var data = google.visualization.arrayToDataTable([
+                                                              ['Country', 'Popularity'],
+                                                              ['Germany', 10],
+                                                              ['United States', 200],
+                                                              ['Brazil', 5],
+                                                              ['Canada', 10],
+                                                              ['France', 10],
+                                                              ['RU', 5],
+                                                              ['China', 10000]
+
+                                                            ]);
+                                                            }
+                         if($("#ddlViewBy :selected").text()=="BuBuJingQing")
+                                                                            {
+                                                                                  var data = google.visualization.arrayToDataTable([
+                                                                                    ['Country', 'Popularity'],
+                                                                                   ['Germany', 10],
+                                                                                    ['United States', 100],
+                                                                                    ['Brazil', 5],
+                                                                                    ['Canada', 10],
+                                                                                    ['France', 10],
+                                                                                    ['RU', 5],
+                                                                                    ['China', 10000]
+
+                                                                                  ]);
+                                                                                  }
+                          if($("#ddlViewBy :selected").text()=="LeJunKai")
+                                                                             {
+                                                                                   var data = google.visualization.arrayToDataTable([
+                                                                                     ['Country', 'Popularity'],
+                                                                                     ['Germany', 10],
+                                                                                     ['United States', 80],
+                                                                                     ['Brazil', 5],
+                                                                                     ['Canada', 10],
+                                                                                     ['France', 10],
+                                                                                     ['RU', 5],
+                                                                                     ['China', 10000]
+
+                                                                                   ]);
+                                                                                   }
+
 
                       var options = {};
 
