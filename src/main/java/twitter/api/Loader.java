d9f82e56b51f035f32ea59c9552624e648373d80
Akhil Kohli <akohli@TDV-GRAPX-AKHIL.local>
2014-11-10 11:01:21 +0530

    commiting

--- src/main/java/twitter/api/Loader.java
+++ src/main/java/twitter/api/Loader.java
@@ -16,38 +16,41 @@ public static void main(String args[])
         try {
             CloudSolrPersistenceLayer.getInstance().init();
             InitializePopularDocuments.init();
+            Aggregator aggregator=new Aggregator();
+            aggregator.aggregateTotal();
+            aggregator.init();
 //            Zap2ItSolrApi.init();
 //            Thread.sleep(10000);
 //            SocialXMLParser.init();
 //            Thread.sleep(2000);
       //      MapSocialWebsitesToShows.init();
 
-            ViewsYoutubeLoader.init();
-            RefactoredTumblrLoader.init();
-            KloutDaemon kloutDaemon=new KloutDaemon();
-            kloutDaemon.init();
-            YoutubeDaemon youtubeDaemon=new YoutubeDaemon();
-            youtubeDaemon.init();
-
-            TorrentzDaemon torrentzDaemon = new TorrentzDaemon();
-            torrentzDaemon.init();
-
-
-            TumblrDaemon tumblrDaemon=new TumblrDaemon();
-            tumblrDaemon.init();
-
-
-
-
-
-            TwitterTweetDaeomon twitterTweetDaeomon=new TwitterTweetDaeomon();
-            twitterTweetDaeomon.init();
-
-
-            new TwitterOneMore().init();
-
-            FacebookDaemon facebookDaemon=new FacebookDaemon();
-            facebookDaemon.init();
+//            ViewsYoutubeLoader.init();
+//            RefactoredTumblrLoader.init();
+//            KloutDaemon kloutDaemon=new KloutDaemon();
+//            kloutDaemon.init();
+//            YoutubeDaemon youtubeDaemon=new YoutubeDaemon();
+//            youtubeDaemon.init();
+//
+//            TorrentzDaemon torrentzDaemon = new TorrentzDaemon();
+//            torrentzDaemon.init();
+//
+//
+//            TumblrDaemon tumblrDaemon=new TumblrDaemon();
+//            tumblrDaemon.init();
+//
+//
+//
+//
+//
+//            TwitterTweetDaeomon twitterTweetDaeomon=new TwitterTweetDaeomon();
+//            twitterTweetDaeomon.init();
+//
+//
+//            new TwitterOneMore().init();
+//
+//            FacebookDaemon facebookDaemon=new FacebookDaemon();
+//            facebookDaemon.init();
 
 
         }
